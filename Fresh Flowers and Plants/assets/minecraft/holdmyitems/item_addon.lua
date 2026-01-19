e225235705d91f25f8f9548dfe779582f062a723
maingvaldsen <maingvaldsen@gmail.com>
2026-01-19 20:58:22 +0100

    Big code cleanup (my ocd noticed sumthn 😭)

--- Fresh Flowers and Plants/assets/minecraft/holdmyitems/item_addon.lua
+++ Fresh Flowers and Plants/assets/minecraft/holdmyitems/item_addon.lua
@@ -28,9 +28,9 @@ local time = P:getAge(player)
 
 
 local function noBlocksPlease(blocks)
-    for _, id in ipairs(blocks) do
-        renderAsBlock:put(id, false)
-    end
+	for _, id in ipairs(blocks) do
+		renderAsBlock:put(id, false)
+	end
 end
 
 local function checkList(list)
@@ -42,162 +42,62 @@ local function checkList(list)
 end
 
 local function particleTickerFirefly(particle, id)
-    local state = fireflyState[id]
+	local state = fireflyState[id]
 
-    local jitterAmp = 0.002
-    local jitterSpeed = 0.1
+	local jitterAmp = 0.002
+	local jitterSpeed = 0.1
 
-    if not state then
-        state = {
-            radius = 0.0015 + math.random() * 0.0025,
-            angle = math.random() * 6.283,
-            speed = 0.02 + math.random() * 0.01,
-            axialTwist = 1 + math.random(),
-            axialAmplitude = 0.001 + math.random() * 0.005,
-            jitterPhaseX = math.random() * 6.283,
-            jitterPhaseY = math.random() * 6.283,
-            jitterPhaseZ = math.random() * 6.283,
-            expirationDate = time + 250,
-        }
+	if not state then
+		state = {
+			radius = 0.0015 + math.random() * 0.0025,
+			angle = math.random() * 6.283,
+			speed = 0.02 + math.random() * 0.01,
+			axialTwist = 1 + math.random(),
+			axialAmplitude = 0.001 + math.random() * 0.005,
+			jitterPhaseX = math.random() * 6.283,
+			jitterPhaseY = math.random() * 6.283,
+			jitterPhaseZ = math.random() * 6.283,
+			expirationDate = time + 250,
+		}
 
-        particle.dx = math.cos(state.angle) * state.radius
-        particle.dy = math.sin(state.angle * state.axialTwist) * state.axialAmplitude
-        particle.dz = math.sin(state.angle) * state.radius
+		particle.dx = math.cos(state.angle) * state.radius
+		particle.dy = math.sin(state.angle * state.axialTwist) * state.axialAmplitude
+		particle.dz = math.sin(state.angle) * state.radius
 
-        fireflyState[id] = state
-    end
+		fireflyState[id] = state
+	end
 
-    state.angle = state.angle + state.speed * deltaTime * 30
+	state.angle = state.angle + state.speed * deltaTime * 30
 
-    local orbitX = math.cos(state.angle) * state.radius
-    local orbitY = math.sin(state.angle * state.axialTwist) * state.axialAmplitude
-    local orbitZ = math.sin(state.angle) * state.radius
+	local orbitX = math.cos(state.angle) * state.radius
+	local orbitY = math.sin(state.angle * state.axialTwist) * state.axialAmplitude
+	local orbitZ = math.sin(state.angle) * state.radius
 
-    state.jitterPhaseX = state.jitterPhaseX + jitterSpeed * deltaTime * 30
-    state.jitterPhaseY = state.jitterPhaseY + jitterSpeed * deltaTime * 30
-    state.jitterPhaseZ = state.jitterPhaseZ + jitterSpeed * deltaTime * 30
+	state.jitterPhaseX = state.jitterPhaseX + jitterSpeed * deltaTime * 30
+	state.jitterPhaseY = state.jitterPhaseY + jitterSpeed * deltaTime * 30
+	state.jitterPhaseZ = state.jitterPhaseZ + jitterSpeed * deltaTime * 30
 
-    particle.dx = orbitX + math.sin(state.jitterPhaseX) * jitterAmp
-    particle.dy = orbitY + math.sin(state.jitterPhaseY) * jitterAmp
-    particle.dz = orbitZ + math.sin(state.jitterPhaseZ) * jitterAmp
+	particle.dx = orbitX + math.sin(state.jitterPhaseX) * jitterAmp
+	particle.dy = orbitY + math.sin(state.jitterPhaseY) * jitterAmp
+	particle.dz = orbitZ + math.sin(state.jitterPhaseZ) * jitterAmp
 
-    if not I:isOf(context.item, Items:get("minecraft:firefly_bush")) then
-        state.expirationDate = 0
-        particle.dead = true
-    end
+	if not I:isOf(context.item, Items:get("minecraft:firefly_bush")) then
+		state.expirationDate = 0
+		particle.dead = true
+	end
 end
 
 -- Whatever you do, do not under any circumstances break this for loop, it WILL cause memoryleaks
 -- unless you remove the entire firefly ticker function
 for id, state in pairs(fireflyState) do
-    if time >= state.expirationDate then
-        fireflyState[id] = nil
-    end
+	if time >= state.expirationDate then
+		fireflyState[id] = nil
+	end
 end
 
 
 
-noBlocksPlease({
-	-- Saplings
-    "minecraft:acacia_sapling",
-    "minecraft:birch_sapling",
-    "minecraft:cherry_sapling",
-    "minecraft:dark_oak_sapling",
-    "minecraft:jungle_sapling",
-    "minecraft:mangrove_propagule",
-    "minecraft:oak_sapling",
-    "minecraft:pale_oak_sapling",
-    "minecraft:spruce_sapling",
-
-	-- More Plants
-	"minecraft:bamboo",
-	"minecraft:vine",
-    "minecraft:azalea",
-    "minecraft:big_dripleaf",
-    "minecraft:flowering_azalea",
-    "minecraft:glow_lichen",
-    "minecraft:hanging_roots",
-    "minecraft:kelp",
-    "minecraft:lily_pad",
-    "minecraft:pale_hanging_moss",
-    "minecraft:sculk_vein",
-    "minecraft:sea_pickle",
-    "minecraft:seagrass",
-    "minecraft:small_dripleaf",
-    "minecraft:spore_blossom",
-    "minecraft:sugar_cane",
-
-	-- Even More Plants
-	"minecraft:short_grass",
-    "minecraft:bush",
-    "minecraft:dead_bush",
-    "minecraft:fern",
-    "minecraft:firefly_bush",
-    "minecraft:large_fern",
-    "minecraft:short_dry_grass",
-    "minecraft:tall_dry_grass",
-    "minecraft:tall_grass",
-
-	-- Mushrooms
-	"minecraft:red_mushroom",
-    "minecraft:brown_mushroom",
-    "minecraft:crimson_fungus",
-    "minecraft:warped_fungus",
-
-	-- Flowers
-	"minecraft:dandelion",
-    "minecraft:allium",
-    "minecraft:azure_bluet",
-    "minecraft:blue_orchid",
-    "minecraft:cactus_flower",
-    "minecraft:closed_eyeblossom",
-    "minecraft:cornflower",
-    "minecraft:lilac",
-    "minecraft:lily_of_the_valley",
-    "minecraft:open_eyeblossom",
-    "minecraft:orange_tulip",
-    "minecraft:oxeye_daisy",
-    "minecraft:peony",
-    "minecraft:pink_tulip",
-    "minecraft:pitcher_plant",
-    "minecraft:poppy",
-    "minecraft:red_tulip",
-    "minecraft:rose_bush",
-    "minecraft:sunflower",
-    "minecraft:torchflower",
-    "minecraft:white_tulip",
-    "minecraft:wither_rose",
-
-	-- Corals
-	"minecraft:tube_coral",
-    "minecraft:brain_coral",
-    "minecraft:brain_coral_fan",
-    "minecraft:bubble_coral",
-    "minecraft:bubble_coral_fan",
-    "minecraft:dead_brain_coral",
-    "minecraft:dead_brain_coral_fan",
-    "minecraft:dead_bubble_coral",
-    "minecraft:dead_bubble_coral_fan",
-    "minecraft:dead_fire_coral",
-    "minecraft:dead_fire_coral_fan",
-    "minecraft:dead_horn_coral",
-    "minecraft:dead_horn_coral_fan",
-    "minecraft:dead_tube_coral",
-    "minecraft:dead_tube_coral_fan",
-    "minecraft:fire_coral",
-    "minecraft:fire_coral_fan",
-    "minecraft:horn_coral",
-    "minecraft:horn_coral_fan",
-    "minecraft:tube_coral_fan",
-
-	-- Nether Plants
-	"minecraft:warped_roots",
-    "minecraft:crimson_roots",
-    "minecraft:nether_sprouts",
-    "minecraft:twisting_vines",
-    "minecraft:weeping_vines",
-})
-
+-- // Great wall of common translations \\ --
 local plantsOnDirt = {
 	"minecraft:acacia_sapling",
 	"minecraft:azalea",
@@ -280,10 +180,37 @@ local vineStuff = {
 	"minecraft:weeping_vines",
 }
 
+-- // No common translations or just none at all \\ --
+local otherStuff = {
+	"minecraft:seagrass",
+	"minecraft:kelp",
+	"minecraft:sugar_cane",
+	"minecraft:lily_pad",
+	"minecraft:vine",
+	"minecraft:spore_blossom",
+	"minecraft:sea_pickle",
+	"minecraft:bamboo",
+	"minecraft:glow_lichen",
+	"minecraft:sculk_vein",
+	"minecraft:red_mushroom",
+	"minecraft:brown_mushroom",
+	"minecraft:crimson_fungus",
+	"minecraft:warped_fungus",
+	"minecraft:twisting_vines",
+}
+
+noBlocksPlease(plantsOnDirt)
+noBlocksPlease(flowers)
+noBlocksPlease(corals)
+noBlocksPlease(vineStuff)
+noBlocksPlease(otherStuff)
+
 
 
 -- // Enourmous if statement of optimisation \\ --
-if (checkList(plantsOnDirt)) then
+if I:isEmpty(item) then
+	return
+elseif (checkList(plantsOnDirt)) then
 	-- // Plants on a block of dirt \\ --
 
 	M:moveY(matrices, 0.05)
@@ -308,19 +235,11 @@ if (checkList(plantsOnDirt)) then
 
 			particleManager:addParticle(particles,
 				false,
-				0, --posX
-				0.3, --posY
-				-0.04, --posZ
-				0, --deltaPosX
-				0, --deltaPosY
-				0, --deltaPosZ
-				0, --rotX
-				0, --rotY
-				0, --rotZ
-				0, --deltaRotX
-				0, --deltaRotY
-				0, --deltaRotZ
-				0.3 + math.random() * 0.1, --size
+				0, 0.3, -0.04,
+				0, 0, 0,
+				0, 0, 0,
+				0, 0, 0,
+				0.3 + math.random() * 0.1,
 				Texture:of("minecraft", "textures/particle/glowing_firefly.png"), "ITEM", hand, "OPACITY", "ADDITIVE", 30, 255, function(particle) particleTickerFirefly(particle, fireflyID) end)
 		end
 	end
@@ -334,26 +253,26 @@ elseif checkList(corals) then
 	-- // Corals \\ --
 
 	M:moveX(matrices, -0.15 * handMirror)
-    M:moveY(matrices, 0.15)
+	M:moveY(matrices, 0.15)
 	M:moveZ(matrices, 0.25)
 
-    M:rotateX(matrices, -65)
+	M:rotateX(matrices, -65)
 elseif checkList(vineStuff) then
 	-- // Weeping Vines, Hanging Roots, Pale Hanging Moss \\ --
 
-    M:moveY(matrices, -0.25)
+	M:moveY(matrices, -0.25)
 
-    M:rotateX(matrices, -90)
-    M:rotateX(matrices, M:clamp(P:getPitch(player) / 2.5, -20, 90) + ptAngle)
+	M:rotateX(matrices, -90)
+	M:rotateX(matrices, M:clamp(P:getPitch(player) / 2.5, -20, 90) + ptAngle)
 	M:rotateZ(matrices, ywAngle * 0.5, 0, -0.13, 0)
 elseif I:isOf(item, Items:get("minecraft:seagrass")) then
 	-- // Seagrass \\ --
 
 	M:moveX(matrices, -0.15 * handMirror)
-    M:moveY(matrices, 0.2)
+	M:moveY(matrices, 0.2)
 	M:moveZ(matrices, 0.425)
 
-    M:rotateX(matrices, -60)
+	M:rotateX(matrices, -60)
 	M:rotateY(matrices, 5 * handMirror)
 	M:rotateZ(matrices, 5 * handMirror)
 
@@ -362,9 +281,9 @@ elseif I:isOf(item, Items:get("minecraft:kelp")) then
 	-- // Kelp \\ --
 
 	M:moveX(matrices, 0.05 * handMirror)
-    M:moveY(matrices, -0.05)
+	M:moveY(matrices, -0.05)
 
-    M:rotateX(matrices, -20)
+	M:rotateX(matrices, -20)
 	M:rotateY(matrices, 8 * handMirror)
 	M:rotateZ(matrices, -10 * handMirror)
 
@@ -380,7 +299,7 @@ elseif I:isOf(item, Items:get("minecraft:lily_pad")) then
 	-- // Lily Pad \\ --
 
 	M:moveX(matrices, -0.15 * handMirror)
-    M:moveY(matrices, 0.1)
+	M:moveY(matrices, 0.1)
 	M:moveZ(matrices, 0.15)
 
 	M:rotateZ(matrices, 90 * handMirror)
@@ -390,16 +309,16 @@ elseif I:isOf(item, Items:get("minecraft:vine")) then
 	-- // Normal Vine \\ --
 
 	M:moveX(matrices, 0.05 * handMirror)
-    M:moveY(matrices, -0.025)
+	M:moveY(matrices, -0.025)
 
-    M:rotateX(matrices, 10)
+	M:rotateX(matrices, 10)
 	M:rotateY(matrices, 10 * handMirror)
 elseif I:isOf(item, Items:get("minecraft:spore_blossom")) then
 	-- // Spore Blossom \\ --
 
-    M:moveY(matrices, -0.2)
-    M:rotateX(matrices, -70)
-    M:rotateX(matrices, M:clamp(P:getPitch(player) / 2.5, -20, 90) + ptAngle)
+	M:moveY(matrices, -0.2)
+	M:rotateX(matrices, -70)
+	M:rotateX(matrices, M:clamp(P:getPitch(player) / 2.5, -20, 90) + ptAngle)
 	M:rotateZ(matrices, ywAngle * 0.5, 0, -0.13, 0)
 elseif I:isOf(item, Items:get("minecraft:sea_pickle")) then
 	M:scale(matrices, 1.2, 1.3, 1.2)
